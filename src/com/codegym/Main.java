8ab302657a9e9fe74538610f4768751a230def21
minhhieu99a <hieunb99@gmail.com>
2022-02-08 19:21:07 +0700

    20soNTdautien

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,29 @@
+package com.codegym;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int count = 0;
+        for (int number = 1; number > 0; number++) {
+            boolean check = true;
+            if (number >= 2) {
+                int i = 2;
+                while (i <= Math.sqrt(number)) {
+                    if (number % i == 0) {
+                        check = false;
+                        break;
+                    }
+                    i++;
+                }
+                if (check) {
+                    System.out.println(number + " ");
+                count++;
+                if (count==20){
+                    break;}
+                }
+            }
+        }
+    }
+}
